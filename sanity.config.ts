7eb686c2feafa94d0263db3143fc98e9e68c9f69
Akash <akash8900@gmail.com>
2025-12-13 13:05:24 +0530

    feat: bootstrap sanity studio

---
+++ sanity.config.ts
@@ -0,0 +1,18 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'S.R. Construction',
+
+  projectId: 't1lt9uvy',
+  dataset: 'production',
+
+  plugins: [structureTool(), visionTool()],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
